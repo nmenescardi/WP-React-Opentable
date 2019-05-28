78d44516405e426ec10d87890494e9359da9c40f
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-05-28 15:47:25 -0500

    add material ui core - add selector

--- app/components/InitialForm.js
+++ app/components/InitialForm.js
@@ -2,7 +2,13 @@ import React, { Component, Fragment } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
+/* import RaisedButton from 'material-ui/RaisedButton';
+ */
+
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
 export default class InitialForm extends Component {
   render() {
@@ -14,6 +20,24 @@ export default class InitialForm extends Component {
             hintText="Enter Your First Name"
             floatingLabelText="First Name"
           />
+          <FormControl>
+            <InputLabel htmlFor="age-simple">Age</InputLabel>
+            <Select
+              /*  value={values.age}
+              onChange={handleChange} */
+              inputProps={{
+                name: 'age',
+                id: 'age-simple'
+              }}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
         </Fragment>
       </MuiThemeProvider>
     );
---
+++ app/utils/initialValues.js
@@ -0,0 +1,23 @@
+const restaurantList = [
+  {
+    id: 1,
+    name: 'New York'
+  },
+  {
+    id: 2,
+    name: 'Miami'
+  }
+];
+
+const timeList = [
+  {
+    id: 1,
+    name: '8:00'
+  },
+  {
+    id: 2,
+    name: '8:30'
+  }
+];
+
+export { restaurantList, timeList };
